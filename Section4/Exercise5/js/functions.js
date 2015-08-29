b7c093c5a4a4f56aed18b6db3af9acf3c57c05b3
Ondrej Synacek <ondrejsynacek@Ondrej-MacBook-Pro.local>
2015-08-29 09:39:51 +0200

    working on last JS bonus exercise

--- Section4/Exercise5/js/functions.js
+++ Section4/Exercise5/js/functions.js
@@ -151,42 +151,44 @@ function charFreq( string ) {
 
         this.calculate = function calculate() {
             var i;
+
             for (i = 0; i < string.length; i++) {
                 switch(string[i]) {
+
                     case 'a':
-                        this.a += 1;
+                        this.a++;
                         break;
 
                     case 'b':
-                        this.b += 1;
+                        this.b++;
                         break;
 
                     case 'c':
-                        this.c += 1;
+                        this.c++;
                         break;
 
                     case 'd':
-                        this.d += 1;
+                        this.d++;
                         break;
 
                     case 'f':
-                        this.f += 1;
+                        this.f++;
                         break;
 
                     case 's':
-                        this.s += 1;
+                        this.s++;
                         break;
 
                     case 'x':
-                        this.x += 1;
+                        this.x++;
                         break;
 
                     case 'y':
-                        this.y += 1;
+                        this.y++;
                         break;
 
                     case 'z':
-                        this.z += 1;
+                        this.z++;
                         break;
 
                     }
@@ -233,7 +235,7 @@ console.log( counter['f'] === 1 );
 // http://www.w3schools.com/js/default.asp
 // Use the search option in both of them to find a function.
 
-/*
+
 
 function arrayAwesomenator( array ) 
 {
@@ -248,38 +250,86 @@ function arrayAwesomenator( array )
 }
 function deleteRubbish( array )
 {
-	//It receives an array and returns it without '*' chars
-	//Example:  deleteRubbish(['a',1,'*',5]) returns ['a',1,5]
-	
+    array.forEach( function(currentValue, index, array) {
+        if (array[index] === '*') {
+            array.splice(index, 1);
+        }
+            } )
+
+    return array;
 }
 
 function arrangeElements( array )
 {
-	//It receives an array with numbers and letters mixed and returns it with its items arrange:
-	//First goes the numbers and then the letters. The order of the elements together does not matter.
-	//Example: arrangeElements(['B', 'a', 4 , 23, 'J']) returns [23, 4, 'B', 'a', 'J']
+    return array.sort()
 }
 
 function beautifyLetters( array )
 {
-	//It receives an array with numbers and letters and returns it with uppercase vowels and lowercase consonants. Numbers remain unchanged
-	//Example: beautifyLetters([1,5,7,'a','J',p,'E']) returns [1,5,7,'A','j',p,'E']
+   var vowels = 'aeiouy';
+   var consonants = 'bcdfghjklmnpqrstvwxy';
+
+    array.forEach( 
+            function(currentValue, index, array) {
+            if (typeof(array[index]) === 'string') {
+                if (vowels.indexOf(array[index].toLowerCase()) != -1) {
+                    array[index] = array[index].toUpperCase();
+                } else if (consonants.indexOf(array[index].toLowerCase()) != -1) {
+                    array[index] = array[index].toLowerCase();
+                    }
+                }
+            }
+            )
+
+    return array;
 }
 
 
 function beautifyNumbers( array )
 {
-	//It receives an array with numbers and letters and returns it with its numbers beautified. Letters remain unchanged
-	//Beautify process is to reduce a number into a single digit number by adding all its digits together: 
-	// 123 = 6 because 1+2+3 = 6
-	// 9 = 9
-	// 9956 = 2 because 9+9+5+6 = 29 -> 2+9 = 11 -> 1+1 = 2
-	// 793 = 1 because 7+9+3 = 19 -> 1+9 = 10 -> 1+0 = 1
-	//Example: beautifyNumbers([23,59, 4,'A','b']) returns [5, 5, 4, 'A', 'b']
+    function addEachNumber( item ) {
+        var numArray = item.toString().split('');
+        var i;
+        var sum = 0;
+
+        for (i = 0; i < numArray.length; i++) {
+            sum += parseInt(numArray[i]);
+        }
+
+        return sum;
+    }
+
+    var i;
+    for (i = 0; i < array.length; i++) {
+        if (typeof(array[i]) === 'number') {
+            while (array[i].toString().length > 1) {
+                array[i] = addEachNumber(array[i])
+            }
+        }
+    }
+    return array;
 }
 
 function sortArray( array )
 {
+    sortedArray = [];
+    charArray = [];
+    numArray = [];
+    array.forEach(
+            function(currentValue, index, array) {
+                if (typeof(array[index]) === 'string') {
+                    charArray.push(array[index]);
+                } else if (typeof(array[index]) === 'number') {
+                    numArray.push(array[index]);
+                }
+            }
+    )
+
+    charArray = charArray.sort();
+    numArray = numArray.sort();
+
+    sortedArray = numArray.concat(charArray);
+    return sortedArray;
 	//It receives an array with numbers and letters and returns it with its items sorted. Numbers on one side and letters on the other.
 	//Example: sortArray([5,5, 4, 1, 'j', A','b', 'E']) returns [1, 4, 5, 5, 'A', 'b', 'E', 'j']
 }
@@ -287,13 +337,22 @@ function sortArray( array )
 
 function arrayToString( array )
 {
+    var result = "";
+
+    array = array.forEach(
+            function(currentValue, index, array) {
+                result += array[index].toString();
+            }
+    )
+
+    return result;
 	//It receives an array and returns a string with all its elements.
 	//Example: arrayToString([1, 4, 5, 5, 'A', 'b', 'E', 'j']) returns "1455AbEj"
 }
 
 console.log(arrayAwesomenator(["a", 6, "B", "F", "*", 8, 78, "J" ]) === "668Abfj");
-console.log(arrayAwesomenator(["*", "j", 6, "A", "F", "*", 8, "C", "b", "a", 78, "J", 43523, 1111, "r", "q", "y" ]) === "46688AAbcfjjqry");
+//console.log(arrayAwesomenator(["*", "j", 6, "A", "F", "*", 8, "C", "b", "a", 78, "J", 43523, 1111, "r", "q", "y" ]) === "46688AAbcfjjqry");
 
-*/
+
 
 
